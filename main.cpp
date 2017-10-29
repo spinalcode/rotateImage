6e57d8502dee57211118b4ed908f9ddcd0a98c65
spinalcode <spinal_cord@yahoo.com>
2017-10-29 11:22:41 +0000

    minor changes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 Pokitto::Core game;
 
+#define HALFTIMES 128
 #define TIMES 256
 #define TIMES2 65536
 
@@ -38,17 +39,13 @@ int main(){
         int COS = cosine[ang];
         int ux,uy;
 
+
         for(int y=0; y<88; y++){
             for(int x=0; x<110; x++){
-
                 int x1 = (x-55)*TIMES;
                 int y1 = (y-44)*TIMES;
-
-//                ux=(x-55) * COS - (y-44) * SIN;
-//                uy=(x-55) * SIN + (y-44) * COS;
-                ux = (x1 * COS - y1 * SIN) / TIMES2;
-                uy = (x1 * SIN + y1 * COS) / TIMES2;
-
+                ux = (x1 * COS - y1 * SIN + HALFTIMES) / TIMES2;
+                uy = (x1 * SIN + y1 * COS + HALFTIMES) / TIMES2;
                 if(ux>-55 && ux <55 && uy >-44 && uy <44){
                     char col = rawData[(ux+55)+112*(87-(uy+44))];
                     game.display.drawPixel(x,y,col);
@@ -57,9 +54,8 @@ int main(){
         }
 
 //        if(ang++>=359) ang=0;
-
-        ang-=2;
-        if(ang<=2) ang=359;
+        ang-=1;
+        if(ang<=1) ang=359;
         game.display.update();
   }
 
